369c0ac92483cbbfa34961ddb02619f81b34f31f
Pawel Chalkowski <chauek@gmail.com>
2025-10-05 12:56:15 +0200

    Updated default translations

--- custom_components/energa_awarie/config_flow.py
+++ custom_components/energa_awarie/config_flow.py
@@ -27,8 +27,8 @@ _LOGGER = logging.getLogger(__name__)
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Energa Awarie."""
-
     VERSION = 1
+    MINOR_VERSION = 1
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
